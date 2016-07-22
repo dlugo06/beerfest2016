839b0f124dbee00c8a2271a69d1662c60ed1edfc
Diego Lugo <diego.lugo.06@gmail.com>
2016-07-21 23:08:37 -0700

    Simple sticky nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get 'boletos', to: 'tickets#index'
 
   root to: 'navigation#home'
-  get 'participantes', to: 'navigation#partners'
-  get 'evento', to: 'navigation#event'
-  get 'mapa', to: 'navigation#map'
-  get 'hoteles', to: 'navigation#hotels'
-  get 'contacto', to: 'navigation#contact'
+  get 'participantes', to: 'navigation#partners', as: 'partners'
+  get 'evento', to: 'navigation#event', as: 'event'
+  get 'mapa', to: 'navigation#map', as: 'map'
+  get 'hoteles', to: 'navigation#hotels', as: 'hotels'
+  get 'contacto', to: 'navigation#contact', as: 'contact'
 end
